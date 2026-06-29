b587f8a972c9cf3152e162f3f2c2893eb0fa73f7
AtsushiTsujita <mozu.077@gmail.com>
2026-06-29 20:59:27 +0900

    feat:AI機能追加途中

--- backend/src/controllers/aiController.ts
+++ backend/src/controllers/aiController.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Response, Request } from "express";
 import { suggestRecipes } from "../services/aiService";
-
-
+import { register, login } from "../services/authService";
+import { AppError } from "../middleware/errorHandler";
 export async function getRecipeSuggestion(req: Request, res: Response, next: NextFunction) {
     try {
         const data = await suggestRecipes();
@@ -11,3 +11,14 @@ export async function getRecipeSuggestion(req: Request, res: Response, next: Nex
     }
 
 }
+
+export async function register(req: Request, res: Response, next: NextFunction) {
+    try {
+        const data = req.body
+        const token = await authService.register(data.email, data.password)
+        return res.json({ success: true, data: { token } })
+    } catch {
+        throw new AppError(401, "エラーが発生しました。");
+    }
+}
+
--- backend/src/services/authService.ts
+++ backend/src/services/authService.ts
@@ -1,16 +1,61 @@
-import { userInfo } from "node:os";
-import { jwt } from "zod/v4";
+
+import { AppError } from "../middleware/errorHandler";
+import prisma from "../lib/prisma";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
 
 export async function register(email: string, password: string) {
 
-    await userInfo = prisma.user.findUnique({
+
+    const userInfo = await prisma.user.findUnique({
         where: { email }
     });
 
-    if (email === userInfo) {
-        prisma.user.create({
-
-        })
+    if (userInfo) {
+        throw new AppError(400, "このメアドは使用されているよ～");
     }
 
+    const saltRounds = 10; // ハッシュ化の度合い値
+    const hashpass = await bcrypt.hash(password, saltRounds);
+
+    const data = await prisma.user.create({
+        data: {
+            email: email,
+            password: hashpass,
+        }
+    });
+
+    const token = jwt.sign(
+        { userId: data.id },
+        process.env.JWT_SECRET!,
+        { expiresIn: "7d" }
+    );
+
+
+    return token;
+}
+
+export async function login(email: string, password: string) {
+
+    const user = await prisma.user.findUnique({
+        where: { email }
+    });
+
+    if (!user) {
+        throw new AppError(401, "パスワードまたはメアドが違います")
+    };
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+        throw new AppError(401, "パスワードが違います")
+    };
+
+    const token = jwt.sign(
+        { userId: user.id },
+        process.env.JWT_SECRET!,
+        { expiresIn: "7d" }
+    );
+
+    return token;
 }
